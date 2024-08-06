3c29cae917160a1d4cbe540fd0bc48c097ac888e
ding.jinzhou <ding.jinzhou@ustcinfo.com>
2024-08-06 16:50:54 +0800

    业务环节逻辑补充

--- engine/src/main/java/com/didiglobal/turbo/engine/common/ChatFlowConstant.java
+++ engine/src/main/java/com/didiglobal/turbo/engine/common/ChatFlowConstant.java
@@ -25,5 +25,26 @@ public interface ChatFlowConstant {
          * 环节绑定的业务能力id
          */
         String BIZ_KEY = "bizKey";
+
+        /**
+         * 环节类型统一key
+         */
+        String NODE_TYPE = "nodeType";
     }
+
+    interface NodeType {
+        // 代码片段
+        String CODE = "code";
+        // 变量环节
+        String VAR = "var";
+        // 意图识别
+        String INTENT = "intent";
+        // 知识库
+        String KNOWLEDGE = "knowledge";
+        // 插件环节
+        String PLUGIN = "plugin";
+        // 大模型环节
+        String LLM = "llm";
+    }
+
 }
