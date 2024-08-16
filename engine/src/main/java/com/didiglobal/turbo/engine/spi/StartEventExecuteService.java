6c2f25e2f0c5c1ac742bd178ee517586698283be
ding.jinzhou <ding.jinzhou@ustcinfo.com>
2024-08-16 11:24:14 +0800

    追加开始环节日志埋点

---
+++ engine/src/main/java/com/didiglobal/turbo/engine/spi/StartEventExecuteService.java
@@ -0,0 +1,21 @@
+package com.didiglobal.turbo.engine.spi;
+
+import com.didiglobal.turbo.engine.common.RuntimeContext;
+
+/**
+ * <p> HookService </p>
+ *
+ * @author lijinghao
+ * @version v1.0
+ * @date 2023/2/16 6:59 PM
+ */
+public interface StartEventExecuteService {
+
+    /**
+     * 环节运行时生命周期钩子
+     *
+     * @param runtimeContext 运行时上下文
+     * @return new infos
+     */
+    void invoke(RuntimeContext runtimeContext);
+}
